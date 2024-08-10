78051f801a365f0cedd03e2ab5fb52c289419e6d
Hassan Iftikhar <hassaniftikhar487@gmail.com>
2024-08-10 12:12:38 +0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
-import Photo from './Components/Photo';
+import Photo from './Components/Photos';
 import Vide from './Components/Vide';
 import Back from './Components/Back';
 import { Routes,Route } from 'react-router-dom';
-import LoginForm from './LoginForm';
+//import LoginForm from './LoginForm';
 
 function App() {
   const [query,setQuery]=useState('Nature');
@@ -16,7 +16,7 @@ function App() {
    <Navbar query={query} setQuery={setQuery}/ > 
   <Routes>
     <Route path='/back' element={<Back/>}/>
-    <Route path='/loginn' element={<LoginForm/>}/>
+    {/* <Route path='/loginn' element={<LoginForm/>}/> */}
     <Route path='/photo' element={<Photo query={query}/>}/>
     <Route path='/video' element={<Vide query={query}/>}/>
   </Routes>
